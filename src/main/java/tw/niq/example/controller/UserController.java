2782431f57b035a63943e9b15f4a2b5536faa9d9
Nick <nick.chen@cmp.com.tw>
2023-05-18 10:36:37 +0800

    Create user REST controller

---
+++ src/main/java/tw/niq/example/controller/UserController.java
@@ -0,0 +1,16 @@
+package tw.niq.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+	@GetMapping("/status")
+	public String status() {
+		return "Working";
+	}
+	
+}
