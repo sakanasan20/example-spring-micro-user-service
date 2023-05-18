f6e37cde67ef78cdefd7f8dbe431b5ce1a2aef9e
Nick <nick.chen@cmp.com.tw>
2023-05-18 13:03:36 +0800

    Configure instance ID and port

--- src/main/java/tw/niq/example/controller/UserController.java
+++ src/main/java/tw/niq/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package tw.niq.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/status")
 	public String status() {
-		return "Working";
+		return "Working on port " + env.getProperty("local.server.port");
 	}
 	
 }
