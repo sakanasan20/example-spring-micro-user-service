9fdebe50b91cf8de68bffe7506fa2eef29f79802
Nick <nick.chen@cmp.com.tw>
2023-05-18 10:03:07 +0800

    Initial Commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMicroUserServiceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMicroUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMicroUserServiceApplication.class, args);
+	}
+
+}
